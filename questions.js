807f503d81e5afc31fd5562e6e4944be65a0b371
Vinitha-2025 <vinithasebastin17@gmail.com>
2026-02-17 19:22:14 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+const questions=[
+    {
+    category:"programming",
+    questions:[
+    {
+    question:"What does HTML stand for?",
+    options:[
+    "Hyper Text Markup Language","Hyper Text Pre Processor","Hyper","Text"
+    ],
+    correctAnswer:1,
+    },
+    {
+    question:"What does CSS stand for?",
+    options:[
+    "Cascading","Style","Cascading Style Sheet","Sheet"
+    ],
+    correctAnswer:3,
+    }
+    ]
+ },
+    {
+    category:"Geography",
+    questions:[
+    {
+    question:"What does heater work?",
+    options:[
+    "Switch On","Switch Off","current","Power"
+    ],
+    correctAnswer:3,
+    },
+    {
+    question:"What do you do ?",
+    options:[
+    "Sleep","eat","work","Shopping"
+    ],
+    correctAnswer:2,
+    }
+    ]
+ },
+    {
+    category:"Mathematics",
+    questions:[
+    {
+    question:"What does she work?",
+    options:[
+    "Church","Office","Home","Hotel"
+    ],
+    correctAnswer:1,
+    },
+    {
+    question:"How do you do ?",
+    options:[
+    "Good","Well","Fine","Going well"
+    ],
+    correctAnswer:4,
+    }
+    ]
+ },
+];
---
+++ script.js
@@ -0,0 +1,147 @@
+const configContainer=document.querySelector(".config-container");
+const quizContainer=document.querySelector(".quiz-container");
+const answerOptions=document.querySelector(".answer-options");
+const nextQuestionBtn=document.querySelector(".next-question-btn");
+const questionStatus=document.querySelector(".question-status");
+const timerDisplay=document.querySelector(".time-duration");
+const resultContainer=document.querySelector(".result-container");
+
+//Quiz state variables
+const QUIZ_TIME_LIMIT=5;
+let currentTime=QUIZ_TIME_LIMIT;
+let timer=null;
+let quizCategory="programming";
+let numberOfQuestions=2;
+let currentQuestion=null;
+const questionsIndexHistory=[];
+let correctAnswersCount=0;
+
+//Display the quiz result and hide the quiz container
+const showQuizResult=() =>{
+    quizContainer.style.display="none";
+    resultContainer.style.display="block";
+
+    const resultText=`You answered <b>${correctAnswersCount}</b> out <b>${numberOfQuestions}</b> Questions correctly. Great effect!`;
+    document.querySelector(".result-message").innerHTML=resultText;
+}
+
+//Clear and reset the timer
+const resetTimer=()=>{
+    clearInterval(timer);
+    currentTime=QUIZ_TIME_LIMIT;
+    timerDisplay.textContent=`${currentTime}s`;
+}
+
+//Initialize and start the timer for the current question
+const startTimer=()=>{
+    timer=setInterval(()=>{
+        currentTime--;
+        timerDisplay.textContent=`${currentTime}s`;
+
+        if(currentTime<=0){
+            clearInterval(timer);
+            highLightCorrectAnswer();
+            nextQuestionBtn.style.visibility="visible";
+            quizContainer.querySelector(".quiz-timer").style.background="#c31402";
+
+//Disable all answer options after one option is selected
+ answerOptions.querySelectorAll(".answer-option").forEach(option=>option.style.pointerEvents="none");   
+
+        }
+    },1000);
+}
+//Fetch a random question from based on the selected category
+const getRandomQuestion = () => {
+    const categoryQuestions = questions.find
+    (cat => cat.category.toLowerCase() === quizCategory.toLowerCase()).questions || [];
+//Shoiw the results if all questions have been did
+if(questionsIndexHistory.length>=Math.min(categoryQuestions.length, numberOfQuestions)){
+    return showQuizResult();
+}
+    //Filter out already asked questions and choose a random one
+    const availableQuestions=categoryQuestions.filter((_, index)=> !questionsIndexHistory.includes(index));
+    const randomQuestion= availableQuestions[Math.floor(Math.random()* availableQuestions.length)];
+    questionsIndexHistory.push(categoryQuestions.indexOf(randomQuestion));
+    return randomQuestion;
+}
+//Highlight the correct answer option and add icon
+const highLightCorrectAnswer=()=>{
+    const correctOption=answerOptions.querySelectorAll(".answer-option")[currentQuestion.correctAnswer-1];
+    correctOption.classList.add("correct");
+    const iconHTML=`<span class="material-symbols-rounded">check_circle</span>`;
+    correctOption.insertAdjacentHTML("beforeend",iconHTML);
+}
+
+//Handle the user's answer selection
+const handleAnswer=(option, answerIndex)=>{
+    clearInterval(timer);
+    const isCorrect=(currentQuestion.correctAnswer-1) === answerIndex;
+    option.classList.add(isCorrect ? 'correct':'incorrect');
+    !isCorrect ? highLightCorrectAnswer(): correctAnswersCount++;
+//Insert icon based on correctness
+    const iconHTML=`<span class="material-symbols-rounded">${isCorrect ? 'check_circle' : 'cancel'}</span>`;
+    option.insertAdjacentHTML("beforeend",iconHTML);    
+
+ //Disable all answer options after one option is selected
+ answerOptions.querySelectorAll(".answer-option").forEach(option=>option.style.pointerEvents="none");   
+
+ nextQuestionBtn.style.visibility='visible';
+}
+
+//Render the current question and its options in the quiz
+const renderQuestion=()=>{
+    currentQuestion=getRandomQuestion();
+    if(!currentQuestion) return;
+    console.log(currentQuestion);
+
+    resetTimer();
+    startTimer();
+
+//Update the UI
+    answerOptions.innerHTML="";
+    nextQuestionBtn.style.visibility="hidden";
+            quizContainer.querySelector(".quiz-timer").style.background="#32313c";
+    document.querySelector(".question-text").textContent=currentQuestion.question;
+    questionStatus.innerHTML=`<b> ${questionsIndexHistory.length}</b> of <b>${numberOfQuestions}</b> Questions`;
+//Create option <li> elements,append them and add click event listeners
+    currentQuestion.options.forEach((option,index) =>{
+        const li=document.createElement("li");
+        li.classList.add("answer-option");
+        li.textContent=option;
+        answerOptions.appendChild(li);
+        li.addEventListener("click",() => handleAnswer(li,index));
+    });
+}
+//Start the quiz and return the random question
+const startQuiz=()=>{
+    configContainer.style.display="none";
+    quizContainer.style.display="block";
+
+//Update the quiz category and number of questions
+quizCategory=configContainer.querySelector(".category-option.active").textContent;
+numberOfQuestions=parseInt(configContainer.querySelector(".question-option.active").textContent);    
+
+    renderQuestion();
+}
+
+//Highlight the selected option on click-category or no of question
+document.querySelectorAll(".category-option,.question-option").forEach(option =>{
+    option.addEventListener("click",()=>{
+        option.parentNode.querySelector(".active").classList.remove("active");
+        option.classList.add("active");
+    });
+});
+
+//Reset the quiz and return the configuration container
+const resetQuiz=()=>{
+    resetTimer();
+    correctAnswersCount=0;
+    questionsIndexHistory.length=0;
+    configContainer.style.display="block";
+    resultContainer.style.display="none";
+}
+
+renderQuestion();
+nextQuestionBtn.addEventListener("click",renderQuestion);
+document.querySelector(".try-again-btn").addEventListener("click",resetQuiz);
+document.querySelector(".start-quiz-btn").addEventListener("click",startQuiz);
